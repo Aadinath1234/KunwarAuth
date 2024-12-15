b83dc57a061f1c1878a8af9076aa8c9c77846756
Aadinath1234 <93751167+Aadinath1234@users.noreply.github.com>
2024-12-15 18:40:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
-app.use(cors({ origin : "http://localhost:5173" , credentials : true}));
+app.use(cors({ origin : "https://kunwarauth-1.onrender.com" , credentials : true}));
 
 app.use(express.json()); // allows us to parse incoming requests : req.body
 app.use(cookieParser()); // allows us to parse incoming cookies 
